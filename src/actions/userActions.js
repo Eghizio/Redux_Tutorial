53467c77c1918ef7b8dc796cb9b9dbfb0b135b1e
Jakub Wąsik <eghizio@gmail.com>
2019-03-27 11:44:49 +0100

    Promises (Async)

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,7 +1,20 @@
 export function setName(name){
+    // return dispatch => {
+    //     //async simulation, prolly a promise
+    //     setTimeout(() => {
+    //         dispatch({
+    //             type: "SET_NAME",
+    //             payload: name
+    //         });
+    //     }, 2000);
+    // }
     return {
         type: "SET_NAME",
-        payload: name
+        payload: new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(name);
+            }, 2000);
+        })
     };
 }
 
--- src/store.js
+++ src/store.js
@@ -1,10 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+//Middleware
 import logger from "redux-logger";
+import thunk from "redux-thunk";
+import promise from "redux-promise-middleware";
+//Reducers
 import mathReducer from "./reducers/mathReducer";
 import userReducer from "./reducers/userReducer";
 
 const store = createStore(
     combineReducers( {mathReducer, userReducer} ), 
-    {}, applyMiddleware( logger ) );
+    {}, 
+    applyMiddleware( logger, thunk, promise ) );
 
 export default store;
